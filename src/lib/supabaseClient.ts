4728f1342842250ac972465d3b8a795195597fbe
septeriodelvan-droid <septeriodelvan@gmail.com>
2026-08-11 11:26:17 +0700

    Delete src directory

--- src/lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-// Menggunakan createBrowserClient agar Dashboard & Menu lain
-// bisa membaca COOKIE login yang sudah dibuat di halaman Login.
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
